07a6a678997c53c10b937df2121f43c2f515e3c9
TitrenMathieu <insectozaure@gmail.com>
2018-01-18 19:40:52 +0100

    update verif modif

--- modif_verif.php
+++ modif_verif.php
@@ -12,5 +12,8 @@
 	 
      header("location:index.php");
  }
- 
+ /* $sql="INSERT INTO messages (contenu, date) VALUES (:contenu, UNIX_TIMESTAMP())";
+        $prep=$pdo->prepare($sql);
+        $prep->bindValue(':contenu', $_POST['message']);
+		*/
  ?>
